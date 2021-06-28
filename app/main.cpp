40b66313d1a9b1b80c703e213a2b52d69660cf6d
navono <navono007@gmail.com>
2021-06-28 16:50:11 +0800

    style: 格式化

--- app/main.cpp
+++ app/main.cpp
@@ -21,8 +21,8 @@
  * CMake definitions (here the version number) from source code.
  */
 int main() {
-  std::cout << "C++ Demo v" << PROJECT_VERSION_MAJOR << "." << PROJECT_VERSION_MINOR << "."
-            << PROJECT_VERSION_PATCH << "." << PROJECT_VERSION_TWEAK << std::endl;
+  std::cout << "C++ Demo v" << PROJECT_VERSION_MAJOR << "." << PROJECT_VERSION_MINOR << "." << PROJECT_VERSION_PATCH
+            << "." << PROJECT_VERSION_TWEAK << std::endl;
 
   folly::fbstring fs("World");
 
