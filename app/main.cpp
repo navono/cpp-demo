480703b95ba0c1b37474b4f9ddfb33c08de1603d
navono <navono007@gmail.com>
2021-06-29 16:34:20 +0800

    feat: 使用 Quill 作为日志库

--- app/main.cpp
+++ app/main.cpp
@@ -8,37 +8,35 @@
 
 #include <fmt/format.h>
 #include <folly/FBString.h>
-#include <spdlog/spdlog.h>
-
-#include <iostream>
 
 #include "config.h"
 #include "ctrl-c.h"
 #include "example.h"
+#include "logger.h"
 
 /*
  * Simple main program that demonstrates how access
  * CMake definitions (here the version number) from source code.
  */
 int main() {
-  std::cout << "C++ Demo v" << PROJECT_VERSION_MAJOR << "." << PROJECT_VERSION_MINOR << "." << PROJECT_VERSION_PATCH
-            << "." << PROJECT_VERSION_TWEAK << std::endl;
+  auto logger = initLogger();
+
+  LOG_INFO(logger, "C++ Demo v{}.{}.{}.{}", PROJECT_VERSION_MAJOR, PROJECT_VERSION_MINOR, PROJECT_VERSION_PATCH,
+           PROJECT_VERSION_TWEAK);
 
   folly::fbstring fs("World");
-
-  // Use the default logger (stdout, multi-threaded, colored)
-  spdlog::info(fmt::format("Hello: {}!", fs.toStdString()));
+  LOG_INFO(logger, "Hello: {}!", fs.toStdString());
 
   std::mutex wait_lock;
   std::condition_variable wait_var;
   const unsigned int kMaxCatches = 2;
   unsigned int catches = 0;
 
-  unsigned int handler_id =
-      CtrlCLibrary::SetCtrlCHandler([&catches, &wait_lock, &wait_var](enum CtrlCLibrary::CtrlSignal event) -> bool {
+  unsigned int handler_id = CtrlCLibrary::SetCtrlCHandler(
+      [&logger, &catches, &wait_lock, &wait_var](enum CtrlCLibrary::CtrlSignal event) -> bool {
         switch (event) {
           case CtrlCLibrary::kCtrlCSignal:
-            spdlog::info("Catch Ctrl+C");
+            LOG_INFO(logger, "Catch Ctrl+C");
         }
         std::lock_guard<std::mutex> locker(wait_lock);
         ++catches;
@@ -46,11 +44,11 @@ int main() {
         return true;
       });
   if (handler_id == CtrlCLibrary::kErrorID) {
-    spdlog::error("Can't set ctrl+c handler");
+    LOG_ERROR(logger, "Can't set ctrl+c handler");
     return 0;
   }
 
-  spdlog::info(fmt::format("Press Ctrl+C {} times", kMaxCatches));
+  LOG_INFO(logger, "Press Ctrl+C {} times", kMaxCatches);
 
   std::unique_lock<std::mutex> locker(wait_lock);
   wait_var.wait(locker, [&catches, kMaxCatches]() { return catches >= kMaxCatches; });
