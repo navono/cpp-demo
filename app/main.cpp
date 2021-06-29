fb934721abf9a1178c15ba2dafb3ce03e50f1605
navono <navono007@gmail.com>
2021-06-29 16:38:37 +0800

    refactor: 使用最新的 demo 分支代码重构

--- app/main.cpp
+++ app/main.cpp
@@ -8,16 +8,38 @@
 
 #include <fmt/format.h>
 #include <folly/FBString.h>
+#include <folly/Format.h>
+#include <folly/Uri.h>
+#include <folly/executors/CPUThreadPoolExecutor.h>
+#include <folly/executors/ThreadedExecutor.h>
+#include <folly/futures/Future.h>
+
+#include <iostream>
 
 #include "config.h"
 #include "ctrl-c.h"
 #include "example.h"
 #include "logger.h"
 
-/*
- * Simple main program that demonstrates how access
- * CMake definitions (here the version number) from source code.
- */
+static void print_uri(const folly::fbstring& address, folly::Future<bool>& exit_future) {
+  while (true) {
+    try {
+      const folly::Uri uri(address);
+      const auto authority = folly::format("The authority from {} is {}", uri.fbstr(), uri.authority());
+      LOG_INFO(quill::get_logger("app"), "{}", authority.str());
+
+      exit_future.wait(std::chrono::milliseconds(2000));
+
+      if (exit_future.hasValue()) {
+        LOG_INFO(quill::get_logger("app"), "exit_future has value");
+        return;
+      }
+    } catch (const std::exception& e) {
+      LOG_ERROR(quill::get_logger("app"), "exit_future has error {}", e.what());
+    }
+  }
+}
+
 int main() {
   auto logger = initLogger();
 
@@ -49,6 +71,36 @@ int main() {
   }
 
   LOG_INFO(logger, "Press Ctrl+C {} times", kMaxCatches);
+
+  //  folly::ThreadedExecutor executor;
+  //  folly::Promise<folly::fbstring> promise;
+  //
+  //  folly::Future<folly::fbstring> future = promise.getSemiFuture().via(&executor);
+  //  folly::Future<folly::Unit> unit = std::move(future).thenValue(print_uri);
+
+  //  promise.setValue("https://conan.io/");
+  //  std::move(unit).get();
+
+  folly::Promise<bool> exit_promise;                           // create promise object
+  folly::Future<bool> exit_future = exit_promise.getFuture();  // create future objects
+
+  folly::CPUThreadPoolExecutor executor(5);
+  folly::Promise<folly::fbstring> promise;
+
+  folly::Future<folly::fbstring> fut = promise.getSemiFuture().via(&executor);
+  //  auto unit = std::move(fut).thenValue(print_uri);
+
+  //  auto unit = std::move(fut).thenValue([=, &exit_future](folly::fbstring&& v) { print_uri(v, exit_future); });
+  std::move(fut).thenValue([=, &exit_future](folly::fbstring&& v) { print_uri(v, exit_future); });
+
+  promise.setValue("https://conan.io/");
+  std::this_thread::sleep_for(std::chrono::seconds(7));  // wait for 7 seconds
+
+  exit_promise.setValue(true);
+  //  std::move(unit).get();
+
+  // 阻塞跟 executor 的执行函数，直到其退出
+  executor.stop();
 
   std::unique_lock<std::mutex> locker(wait_lock);
   wait_var.wait(locker, [&catches, kMaxCatches]() { return catches >= kMaxCatches; });
