014544e244e5241e76f5eb9154652637c6adf733
supcon <navono007@gmail.com>
2023-02-24 13:51:19 +0800

    chore: clang-format 修改

--- source/demo/src/main.cpp
+++ source/demo/src/main.cpp
@@ -15,16 +15,15 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
 #else
 int main(int argc, char **argv) {
   Config::initializeArgs(argc, argv);
-//  NUIKIT_DEBUG(fmt::format("optDaemon: {}, optDebug: {}", Config::optDaemon, Config::optDebug));
+  //  NUIKIT_DEBUG(fmt::format("optDaemon: {}, optDebug: {}", Config::optDaemon, Config::optDebug));
 
 #endif
 
-    zmq::context_t ctx;
-    zmq::socket_t sock(ctx, zmq::socket_type::push);
-    sock.bind("inproc://test");
-    sock.send(zmq::str_buffer("Hello, world"), zmq::send_flags::dontwait);
+  zmq::context_t ctx;
+  zmq::socket_t sock(ctx, zmq::socket_type::push);
+  sock.bind("inproc://test");
+  sock.send(zmq::str_buffer("Hello, world"), zmq::send_flags::dontwait);
 
-
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
 }
