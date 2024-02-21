896ee5cbc30f01c81ad353c84e78549016a98a96
Darkness <navono007@gmail.com>
2024-02-21 19:22:42 +0800

    feat: 增加 GUI 隐藏模式下事件循环和窗口过程

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,26 @@
 #include "utils/XLogger.h"
-#include <fmt/core.h>
 
 #ifdef _WIN32
 #include <Windows.h>
+#include <afx.h>
+#include <afxwin.h>
 #endif
 
-DWORD g_dwMainThreadId;
-BOOL WINAPI ConsoleCtrlHandler(
-    DWORD dwCtrlType) {
-  if (dwCtrlType == CTRL_CLOSE_EVENT) {
-    //PostQuitMessage(0);
-    PostThreadMessage(g_dwMainThreadId, WM_QUIT, 0, 0);
-    return TRUE;
+static const wchar_t m_szName[] = L"窗口";
+// 主窗口回调函数 如果直接用 DefWindowProc, 关闭窗口时不会结束消息循环
+static LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+  switch (uMsg) {
+    case WM_DESTROY:
+      PostQuitMessage(0);// 关闭窗口时发送WM_QUIT消息结束消息循环
+      break;
+    case WM_NCCREATE: {
+      XLOG_TRACE("window initialized");
+      break;
+    }
+    default:
+      return DefWindowProc(hWnd, uMsg, wParam, lParam);
   }
-  return FALSE;
+  return 0;
 }
 
 #ifdef _WIN32
@@ -27,17 +34,32 @@ int main(int argc, char **argv) {
 #endif
 
   XLOG_TRACE("Hello");
-  fmt::print("Hello");
 
-  g_dwMainThreadId = GetCurrentThreadId();
-  SetConsoleCtrlHandler(ConsoleCtrlHandler, TRUE);
-  //TODO: Any initialize code here:
-  MSG msg;
-  while (GetMessage(&msg, NULL, 0, 0)) {
-    TranslateMessage(&msg);
-    DispatchMessage(&msg);
+  WNDCLASS wc;
+  memset(&wc, 0, sizeof(WNDCLASS));
+  wc.style = CS_VREDRAW | CS_HREDRAW;
+  wc.lpfnWndProc = (WNDPROC) WindowProc;
+  wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+  wc.hbrBackground = (HBRUSH) (COLOR_WINDOW);
+  wc.lpszClassName = m_szName;
+  RegisterClass(&wc);// 登记窗口类
+
+  HWND hWnd;
+  hWnd = CreateWindow(m_szName, m_szName,
+                      WS_OVERLAPPEDWINDOW,
+                      0,                                  // 窗口 X 坐标
+                      0,                                  // 窗口 Y 坐标
+                      0,                                  // 窗口宽度
+                      0,                                  // 窗口高度
+                      nullptr, nullptr, nullptr, nullptr);// 创建窗口
+  ShowWindow(hWnd, SW_HIDE);                              // 隐藏窗口
+
+  MSG sMsg;
+  while (int ret = GetMessage(&sMsg, nullptr, 0, 0)) {// 消息循环
+    if (ret != -1) {
+      TranslateMessage(&sMsg);
+      DispatchMessage(&sMsg);
+    }
   }
-  SetConsoleCtrlHandler(ConsoleCtrlHandler, FALSE);
-  //TODO: Any code to release resources here:
   return 0;
 }
