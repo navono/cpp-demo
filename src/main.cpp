f3dc74d6c234bdbdc8bd443686f5d434bfa739ff
Darkness <navono007@gmail.com>
2024-03-04 19:17:54 +0800

    feat: 增加 POCO 的 thread 和线程间通信的示例

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
-//#include <BS_thread_pool.hpp>
-//#include <BS_thread_pool_utils.hpp>
 #include <chrono>// std::chrono
 //#include <fmt/core.h>
 #include <Poco/DigestStream.h>
 #include <Poco/MD5Engine.h>
+#include <Poco/Notification.h>
+#include <Poco/NotificationCenter.h.>
+#include <Poco/NotificationQueue.h>
+#include <Poco/ThreadPool.h>
 #include <hv/HttpServer.h>
 #include <thread>// std::this_thread
 
@@ -23,19 +25,53 @@
 //int main(int argc, char **argv) {
 //#endif
 
-//BS::synced_stream sync_out;
-//BS::thread_pool pool(4);
-//
-//void sleep_half_second(const int i) {
-//  std::this_thread::sleep_for(std::chrono::milliseconds(500));
-//  sync_out.println("Task ", i, " done.");
-//}
-//
-//void monitor_tasks() {
-//  sync_out.println(pool.get_tasks_total(), " tasks total, ", pool.get_tasks_running(), " tasks running, ", pool.get_tasks_queued(), " tasks queued.");
-//}
-
 using namespace hv;
+
+// 定义一个自定义的通知类
+class DataNotification : public Poco::Notification {
+ public:
+  explicit DataNotification(int data) : _data(data) {}
+  int data() const { return _data; }
+
+ private:
+  int _data;
+};
+
+// 定义一个特殊的退出通知类
+class StopNotification : public Poco::Notification {};
+
+class MyTask : public Poco::Runnable {
+ public:
+  explicit MyTask(Poco::NotificationQueue &queue) : _queue(queue), _running(true) {}
+
+  void run() override {
+    XLOG_TRACE("MyTask run, waiting data from main thread...");
+    while (_running) {
+      Poco::Notification::Ptr pNotification(_queue.waitDequeueNotification());
+
+      if (pNotification) {
+        if (dynamic_cast<StopNotification *>(pNotification.get())) {
+          // 收到退出通知，停止运行
+          _running = false;
+          continue;
+        }
+
+        auto *pDataNotification = dynamic_cast<DataNotification *>(pNotification.get());
+        if (pDataNotification) {
+          // 处理数据
+          XLOG_TRACE("Received data from main thread: {}", pDataNotification->data());
+        }
+      }
+    }
+
+    XLOG_TRACE("Exit MyTask run...");
+  }
+
+ private:
+  Poco::NotificationQueue &_queue;
+  bool _running;
+};
+
 int main(int argc, char **argv) {
   XLOG_TRACE("Hello");
 
@@ -46,9 +82,20 @@ int main(int argc, char **argv) {
   auto digest = Poco::DigestEngine::digestToHex(md5.digest());
   XLOG_TRACE("digest: {}", digest);
 
+  Poco::NotificationQueue queue;
+  // 创建并启动子线程
+  MyTask worker(queue);
+  Poco::Thread thread;
+  thread.start(worker);
+
   HttpService router;
-  router.GET("/ping", [](HttpRequest *req, HttpResponse *resp) {
+  router.GET("/ping", [&queue](HttpRequest *req, HttpResponse *resp) {
+    queue.enqueueNotification(new DataNotification(123));
     return resp->String("pong");
+  });
+  router.GET("/stop", [&queue](HttpRequest *req, HttpResponse *resp) {
+    queue.enqueueNotification(new StopNotification());
+    return resp->String("stopped");
   });
 
   XLOG_TRACE("http server running at: 8080");
@@ -56,16 +103,6 @@ int main(int argc, char **argv) {
   server.setPort(8080);
   server.setThreadNum(4);
   server.run();
-
-  //  pool.wait();
-  //  pool.detach_sequence(0, 12, sleep_half_second);
-  //  monitor_tasks();
-  //  std::this_thread::sleep_for(std::chrono::milliseconds(750));
-  //  monitor_tasks();
-  //  std::this_thread::sleep_for(std::chrono::milliseconds(500));
-  //  monitor_tasks();
-  //  std::this_thread::sleep_for(std::chrono::milliseconds(500));
-  //  monitor_tasks();
 
   return 0;
 }
